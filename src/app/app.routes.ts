0ceca89c06391e26282913bfc23659921f63220f
LuisF-1995 <luis-1995@live.com.ar>
2023-11-26 22:21:17 -0500

    Estructura basica del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { dashboard, login, register } from '../constants/Routes';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  login,
+  register,
+  dashboard,
+  {
+    path:'**',
+    component:ErrorPageComponent
+  }
+];
